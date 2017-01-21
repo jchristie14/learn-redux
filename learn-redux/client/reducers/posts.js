a91f4af184365cc558d86b43a4f0c00f90e3efae
John Christie <omeara.christie@gmail.com>
2017-01-21 10:42:03 -0500

    Finish likes reducer

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,6 +1,19 @@
 // a reducer takes in two things: the action & copy of current state
 
 function posts(state = [], action) {
+	switch(action.type) {
+		case 'INCREMENT_LIKES' :
+      console.log(`incrementing likes`);
+      const i = action.index;
+      return [
+        ...state.slice(0,i),//before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1),//after the one we are updating
+      ]
+    //return updated state
+		default:
+		  return state;
+	}
 	console.log(state, action);
 	return state;
 }
